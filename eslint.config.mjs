b1911e2fc246c8ac6e401af963b0c501c73ecb68
zhangxb <863232387@qq.com>
2026-03-19 10:11:53 +0800

    feat(eslint): 添加自动导入配置并更新tsconfig
    在eslint配置中添加auto-import的全局变量配置
    更新tsconfig.app.json以包含自动生成的类型声明文件

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,15 @@
 import { react } from 'eslint-config-ali';
 import prettier from 'eslint-plugin-prettier/recommended';
+import autoImportConfig from './.eslintrc-auto-import.json';
 
-export default [...react, prettier];
+export default [
+  ...react,
+  prettier,
+  {
+    languageOptions: {
+      globals: {
+        ...autoImportConfig.globals,
+      },
+    },
+  },
+];
